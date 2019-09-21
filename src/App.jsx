77a51500bc0d811b8bed6d02c37275ae5b9d350a
adamsandwich <adamsandwich@outlook.com>
2019-09-21 23:03:26 +0800

    :sparkles: feat: open and delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import _ from 'lodash'
 import dayjs from 'dayjs'
-import { Button, Paper } from '@material-ui/core'
+import { Button, Paper, Typography, Card, CardActionArea, CardActions, CardContent, Divider, FormControlLabel, Switch } from '@material-ui/core'
 import { Save as SaveIcon, Delete as DeleteIcon } from '@material-ui/icons'
 import { getTabs, setTabs, getCurrentTabs, createTab } from './utils/storage'
 import './App.css'
@@ -22,16 +22,21 @@ class App extends Component {
     this.state = {
       currentTabs: [],
       savedTabs: [],
+      checkOpenAndDelete: false,
     }
   }
   renderLinkItem = element => {
     return (
       <Paper className="link" key={element.url}>
         <span className="favIcon" style={{ backgroundImage: `url(${element.favIconUrl})` }}></span>
-        <a className="link-href" onClick={() => createTab(element)}>{element.title}</a>
+        <a className="link-href" onClick={() => this.createTab(element)}>{element.title}</a>
         <DeleteIcon className="link-delete wobble fast infinite animated" onClick={() => this.removeTab(element.url)}></DeleteIcon>
       </Paper>
     )
+  }
+  createTab = (element) => {
+    createTab(element)
+    this.state.checkOpenAndDelete ? this.removeTab(element.url) : null
   }
   componentDidMount() {
     getTabs()
@@ -72,32 +77,56 @@ class App extends Component {
         })
       })
   }
+  handleChange = (name) => (event) => {
+    this.setState({ [name]: event.target.checked });
+  }
   render() {
-    const { currentTabs, savedTabs } = this.state
+    const { currentTabs, savedTabs, checkOpenAndDelete } = this.state
     return (
       <div>
-        <h1>Akai</h1>
-        <div id="buttonArea">
-          <Button variant="contained" size="small" onClick={this.setCurrentTabs}>
-            <SaveIcon style={{ marginRight: '8px' }} />
-            保存
-          </Button>
-        </div>
+        <Card className="title">
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">Akai</Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" onClick={this.setCurrentTabs}>
+              <SaveIcon style={{ marginRight: '8px' }} />
+              保存
+            </Button>
+            <FormControlLabel
+              style={{ padding: '0 8px' }}
+              value="end"
+              control={
+                <Switch
+                  color="primary"
+                  size="small"
+                  checked={checkOpenAndDelete}
+                  onChange={this.handleChange('checkOpenAndDelete')}
+                />
+              }
+              label="打开并删除"
+              labelPlacement="end"
+            />
+          </CardActions>
+        </Card>
         <div id="currentTabs" className="current tabs">
-          <h2>当前 tabs</h2>
+          <Typography variant="subtitle1"> 当前 tabs</Typography>
           {
             !currentTabs.length ? null : currentTabs.map(renderImmutableLinkItem)
           }
         </div>
         <div id="savedTabs" className="saved tabs">
-          <h2>保存的 tabs</h2>
+          <Typography variant="subtitle1">保存的 tabs</Typography>
           {
             !savedTabs.length ? null : savedTabs.map((elements) => {
               if (!elements.length) return
               const time = elements[0].time
               return (
                 <div className="links-container" key={time}>
-                  <h3>{dayjs.unix(time).format('YYYY-MM-DD HH:mm:ss')}</h3>
+                  <Typography className="subtitle2" variant="subtitle2">{dayjs.unix(time).format('YYYY-MM-DD HH:mm:ss')}</Typography>
+                  <Divider />
                   {
                     !elements.length ? null : elements.map((this.renderLinkItem))
                   }
